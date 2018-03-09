2e4c9bc48b5d33baa43189926d49d7c3da816a04
Friedrich Alexander <friedalex@group.riehle.org>
2018-03-09 15:51:20 +0100

    Commented out example class

--- src/main/java/com/unieins/happy/Credentials_EXAMPLE.java
+++ src/main/java/com/unieins/happy/Credentials_EXAMPLE.java
@@ -1,6 +1,7 @@
 package com.unieins.happy;
 
-public class Credentials {
+/*public class Credentials {
 	public static final String SENDGRID_PW = "xxx";
 	public static final String SENDGRID_USER = "xxx";
 	}
+*/
